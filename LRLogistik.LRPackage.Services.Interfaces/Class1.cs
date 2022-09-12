8f7d37c274c65d41b3ad87c6abad7d134ff8e5d4
johannesrathgeb <jrathgeb2001@gmail.com>
2022-09-12 21:59:46 +0200

    Initial commit

---
+++ LRLogistik.LRPackage.Services.Interfaces/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRLogistik.LRPackage.Services.Interfaces
+{
+    internal class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Test");
+
+        }
+        
+    }
+}
