85a20d96704134fe6e804d024582d4bb434c572c
xrox555 <flo.le@gmx.at>
2022-10-24 12:17:27 +0200

    Converters implemented

---
+++ LRLogistik.LRPackage.BusinessLogic/Converters/GeoJsonConverter.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRLogistik.LRPackage.BusinessLogic.Converters
+{
+    public class GeoJsonConverter :
+            IValueConverter<string, NetTopologySuite.Geometries.Geometry>,
+            IValueConverter<NetTopologySuite.Geometries.Geometry, string>
+    {
+        public NetTopologySuite.Geometries.Geometry Convert(string source, ResolutionContext context)
+        {
+            var serializer = NetTopologySuite.IO.GeoJsonSerializer.CreateDefault();
+            var feature = (Feature)serializer.Deserialize(new StringReader(source), typeof(Feature));
+            return feature?.Geometry;
+        }
+
+        public string Convert(Geometry sourceMember, ResolutionContext context)
+        {
+            Feature feature = new(sourceMember, null);
+
+            var serializer = GeoJsonSerializer.CreateDefault();
+
+            StringWriter writer = new();
+            serializer.Serialize(writer, feature);
+            writer.Flush();
+
+            return writer.ToString();
+        }
+    }
+}
