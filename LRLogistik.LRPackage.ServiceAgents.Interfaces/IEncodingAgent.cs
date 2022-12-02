143ea9597713b4316124639f0847b37325d8d306
your name <75954553+ledererflorian@users.noreply.github.com>
2022-12-02 13:40:50 +0100

    added Service Agents classes

---
+++ LRLogistik.LRPackage.ServiceAgents.Interfaces/IEncodingAgent.cs
@@ -0,0 +1,15 @@
+﻿
+using LRLogistik.LRPackage.BusinessLogic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRLogistik.LRPackage.ServiceAgents.Interfaces
+{
+    public interface IEncodingAgent
+    {
+        //public GeoCoordinate EncodeAddress(Address a);
+    }
+}
